7f09f232af8d274f7572d1d982ef10c5553f7a10
YuanXuDong <yxd9187@163.com>
2018-11-14 19:46:42 +0800

    init Vue

---
+++ src/components/home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        Hello Vue!
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
